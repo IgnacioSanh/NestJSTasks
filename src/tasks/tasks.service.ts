a2c6bcb59590b3d08c129cad195b8e1158789b74
Ignacio Sanhueza <ignacio.sanhueza.l@gmail.com>
2022-11-19 03:05:04 -0300

    Tasks module

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const task: Task = {
+      ...createTaskDto,
+      id: uuid(),
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+
+    return task;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus) {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+}
